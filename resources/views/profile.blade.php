813defaf780761e97cb033c4cb3346772b6fe135
Conor Macpherson <cmacpherson1766@conestogac.on.ca>
2020-12-02 20:34:27 -0500

    Add basic profile page with nav bar link.

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    @if (empty($user))
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                    <p>This profile does not exist</p>
+            </div>
+        </div>
+    </div>
+    @else
+    <div class="row justify-content-center">
+        <div class="col-md-2" id="user-details">
+            @if (empty($user->profileImage))
+                <img src="{{URL('/images/blank-profile-picture.png')}}" height="100" width="100">
+            @else
+                <img src="{{$user->profileImage}}" height="100" width="100">
+            @endif
+            <p>{{$user->name}}</p>
+            <p>Following: {{$user->followingCount}}</p>
+            <p>Followers: {{$user->followerCount}}</p>
+        </div>
+        <div class="col-md-8">
+            <div class="card">
+                    Feed
+            </div>
+        </div>
+    </div>
+    @endif
+</div>
+@endsection
