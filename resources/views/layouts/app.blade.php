26707cf5a66201644606d181d7377af6b9ec25fb
Trevor <tallain88@gmail.com>
2020-12-03 18:28:38 -0500

    added in posts view and migration along with creating new posts right now just shows request data still have to hook it up to DB

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,10 +52,18 @@
                                 </li>
                             @endif
                         @else
+                            <li>
+                                <a class="navbar-brand" href="{{ route('posts') }}">
+                                    Posts
+                                </a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
+
+
+
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
@@ -69,6 +77,7 @@
                                     </form>
                                 </div>
                             </li>
+
                         @endguest
                     </ul>
                 </div>
