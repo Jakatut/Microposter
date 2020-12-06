bcf655130558b950e2590c23e637be28539f37fc
Conor Macpherson <cmacpherson1766@conestogac.on.ca>
2020-12-05 22:19:59 -0500

    Remove conflict artifact.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,7 +4,6 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            @include('postCard')
             <div class="card">
                 <div class="card-header">{{ __('Posts Dashboard') }}
                 	
