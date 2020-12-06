3439d1d4e0658c2bde486b3ced28bfb25a0733ad
Conor Macpherson <cmacpherson1766@conestogac.on.ca>
2020-12-06 14:23:00 -0500

    Remove create post button from profile page if the profile does not belong to the user.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -26,7 +26,42 @@
             @endif
         </div>
         <div class="col-md-8">
-            @include('postCard')
+            <div class="card">
+                <div class="card-header">{{ __('Posts Dashboard') }}
+                    
+                </div>
+            
+                <div class="card-body">
+                    
+                    <div class="form-group row">
+                        
+                        @csrf
+                        
+                        @if (Auth::user()->id === $user->id)
+                        <div class="col-4 offset-8">
+                            <a type="submit" class="btn btn-primary" href="{{ route('newPost') }}">
+                                {{ __('Create New') }}
+                            </a>
+                        </div>
+                        @endif
+                    </div>
+            
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @else
+                        @foreach($posts as $post)
+                        <div>
+                            <a href="{{ route('posts.viewPost', $post->id)}}">{{$post->title}}</a>
+                            <!-- {{$post->content}} -->
+                        </div>
+                        @endforeach
+                    @endif
+            
+                    
+                </div>
+            </div>
         </div>
     </div>
 </div>
