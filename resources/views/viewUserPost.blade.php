9d1e4e0e83165005de270aa9b417caf137477d87
Trevor <tallain88@gmail.com>
2020-12-06 19:07:32 -0500

    added in share functionality

--- resources/views/viewUserPost.blade.php
+++ resources/views/viewUserPost.blade.php
@@ -20,11 +20,15 @@
                             <input type="hidden" name="id" value="{{ $post->id }}">
                             <button type="submit" class="btn btn-primary">
                                 <span style="color:white">
-                                <i class="fa fa-thumbs-up"></i> Like
-                                    
-
+                                    <i class="fa fa-thumbs-up"></i> Like
                                 </span>
                             </button>
+                            <a  class="btn btn-primary" data-toggle="modal"  data-backdrop="static" data-keyboard="false" data-target="#myModal">
+                                <span style="color:white">
+                                    <i class="fa fa-share"></i>
+                                </span>
+                                Share
+                            </a>
                         </form>
                     @else
                         <form action="{{ route('dislike')}}" method="post">
@@ -38,14 +42,52 @@
                                 </span>
                                 {{ $post->likes()->count() }} likes
                             </button>
+                            <a class="btn btn-primary" data-toggle="modal"  data-backdrop="static" data-keyboard="false" data-target="#myModal">
+                                <span style="color:white">
+                                    <i class="fa fa-share"></i>
+                                </span>
+                                Share
+                            </a>
                         </form>
-                        <!-- <button class="btn btn-secondary" disabled>
-                            {{ $post->likes()->count() }} likes
-                        </button> -->
+                        
                     @endif
+                    
                 </div>
             </div>
         </div>
     </div>
+    <!-- The Modal -->
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Share Link</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body" >
+        <mark><input id="post-url" value="{{Request::url()}}" readonly></mark>
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-danger" id="copy-button" onclick="copyToClipboard('post-url')"> Copy </button>
+      </div>
+
+    </div>
+  </div>
 </div>
+</div>
+<script>
+     function copyToClipboard(id) {
+        document.getElementById(id).select();
+        document.execCommand('copy');
+        document.getElementById('copy-button').innerText = 'Copied!';
+    }
+</script>
+
 @endsection
